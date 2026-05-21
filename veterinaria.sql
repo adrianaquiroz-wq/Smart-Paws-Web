54e42ff06ea101296691379e6efa064ecee790be
adrianaquiroz-wq <adriana.quiroz@ucb.edu.bo>
2026-05-20 21:52:56 -0400

    nueva DN

--- veterinaria.sql
+++ veterinaria.sql
@@ -1,9 +1,31 @@
-
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 20-05-2026 a las 05:39:59
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `veterinaria`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `atenciones`
+--
 
 CREATE TABLE `atenciones` (
   `id_atencion` int(11) NOT NULL,
@@ -18,7 +40,11 @@ CREATE TABLE `atenciones` (
   `temperatura` decimal(4,1) DEFAULT NULL,
   `frecuencia_cardiaca` int(11) DEFAULT NULL,
   `tratamiento` text DEFAULT NULL,
-  `observaciones` text DEFAULT NULL
+  `observaciones` text DEFAULT NULL,
+  `id_mascota` int(11) NOT NULL,
+  `tipo_atencion` varchar(20) NOT NULL,
+  `hora_inicio` time NOT NULL,
+  `hora_fin` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -38,6 +64,22 @@ CREATE TABLE `citas` (
   `carnetVet` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `citas`
+--
+
+INSERT INTO `citas` (`id_cita`, `id_mascota`, `carnetDue`, `fecha`, `hora`, `motivo`, `estado`, `carnetVet`) VALUES
+(1, 17, 55555, '2026-05-20', '09:00:00', 'Control rutina mañanera.', 'Cancelada', 9244226),
+(2, 17, 55555, '2026-05-20', '15:00:00', 'Contro rutinario tarde.', 'Cancelada', 9244226),
+(3, 17, 55555, '2026-05-22', '11:00:00', 'Lavado de pelo.', 'Cancelada', 555666),
+(4, 17, 55555, '2026-05-20', '10:00:00', 'Bañadita.', 'Cancelada', 9244226),
+(5, 17, 55555, '2026-05-20', '09:00:00', 'Limpieza bucal.', 'Cancelada', 9244226),
+(6, 17, 55555, '2026-05-21', '14:00:00', 'Corte de pelo.', 'Pendiente', 9244226),
+(7, 17, 55555, '2026-05-19', '20:30:00', 'Dolor de barriga.', 'Pendiente', 9244226),
+(8, 17, 55555, '2026-05-19', '23:30:00', 'Revisión de ojos.', 'Pendiente', 9244226),
+(9, 17, 55555, '2026-05-19', '23:45:00', 'blsbla', 'Pendiente', 9244226);
+
+-- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `clientes`
@@ -49,7 +91,23 @@ CREATE TABLE `clientes` (
 
 --
 -- Volcado de datos para la tabla `clientes`
--
+--
+
+INSERT INTO `clientes` (`carnetDue`) VALUES
+(13579),
+(55555),
+(313131),
+(666999),
+(777888),
+(9258589),
+(333555777),
+(654654654),
+(1020304050),
+(2147483647);
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `clientes_mascotas`
 --
 
@@ -62,6 +120,23 @@ CREATE TABLE `clientes_mascotas` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Volcado de datos para la tabla `clientes_mascotas`
+--
+
+INSERT INTO `clientes_mascotas` (`id_registroMasc`, `id_cliente`, `id_mascota`, `fecha_inicio`, `fecha_fin`) VALUES
+(7, 666999, 8, '2026-05-13', NULL),
+(8, 666999, 9, '2026-05-13', NULL),
+(9, 777888, 10, '2026-05-13', NULL),
+(10, 2147483647, 11, '2026-05-14', NULL),
+(11, 654654654, 12, '2026-05-14', NULL),
+(12, 666999, 13, '2026-05-14', NULL),
+(13, 333555777, 14, '2026-05-14', NULL),
+(14, 333555777, 15, '2026-05-14', NULL),
+(15, 13579, 16, '2026-05-18', NULL),
+(16, 55555, 17, '2026-05-19', NULL),
+(17, 1020304050, 18, '2026-05-19', NULL);
+
+-- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `colores`
@@ -110,7 +185,12 @@ CREATE TABLE `compras` (
 --
 -- Volcado de datos para la tabla `compras`
 --
- --------------------------------------------------------
+
+INSERT INTO `compras` (`id_compra`, `id_producto`, `cantidad`, `fecha`, `costo`, `id_mascota`, `carnetDue`) VALUES
+(1, 7, 1, '0000-00-00', 30.00, NULL, 333555777),
+(2, 1, 1, '0000-00-00', 185.00, NULL, 13579);
+
+-- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `especies`
@@ -156,6 +236,21 @@ CREATE TABLE `mascotas` (
 -- Volcado de datos para la tabla `mascotas`
 --
 
+INSERT INTO `mascotas` (`id_mascota`, `nombre`, `id_color`, `id_raza`, `fecha_nacimiento`, `peso`, `tamano`, `descripcion`, `alergias`, `foto`, `estado`) VALUES
+(8, 'Chimuelo', 1, 6, '2025-01-01', 6.60, 'Mediano', '', '', 'img/mascotas/1778678431_gatoSiames.jpg', 'activo'),
+(9, 'Bobby', 2, 12, '2026-01-01', 4.80, 'Pequeño', '', '', 'img/mascotas/1778704035_canario.jpg', 'activo'),
+(10, 'ABBA', 1, 14, '2025-01-01', 8.10, 'Mediano', '', '', 'img/mascotas/1778723169_iguana.jpg', 'activo'),
+(11, 'Yisu', 3, 2, '2016-01-01', 15.30, 'Pequeño', '', '', 'img/mascotas/1778760470_bulldog.jpg', 'activo'),
+(12, 'Diana', 2, 6, '2020-01-01', 0.00, 'Mediano', '', '', 'img/mascotas/1778763140_gatoSiames.jpg', 'activo'),
+(13, 'Cuellerin', 2, 10, '2025-10-10', 4.60, 'Mediano', '', '', NULL, 'activo'),
+(14, 'Pepito', 1, 12, '2025-01-01', 4.20, 'Mediano', '', '', 'img/mascotas/1778764538_canario.jpg', 'activo'),
+(15, 'Yogurt', 1, 11, '2025-01-01', 7.90, 'Pequeño', '', '', 'img/mascotas/1778769358_conejoCaLeon.jpg', 'activo'),
+(16, 'Lorinho', 1, 13, '2026-01-01', 4.60, 'Mediano', '', '', 'img/mascotas/1779128211_loro.jpg', 'activo'),
+(17, 'Alvin', 3, 4, '2025-01-01', 5.80, 'Pequeño', '', '', 'img/mascotas/1779220049_poodle.jpg', 'activo'),
+(18, 'koala', 1, 6, '2025-10-01', 10.40, 'Mediano', '', '', 'img/mascotas/1779229419_gatoSiames.jpg', 'activo');
+
+-- --------------------------------------------------------
+
 --
 -- Estructura de tabla para la tabla `personas`
 --
@@ -173,7 +268,22 @@ CREATE TABLE `personas` (
 --
 -- Volcado de datos para la tabla `personas`
 --
---------------------------------------------------------
+
+INSERT INTO `personas` (`carnet`, `nombre`, `apellido`, `celular`, `direccion`, `usuario`, `contrasena`) VALUES
+(13579, 'Froilan ', 'Gutierrez', NULL, 'Calle Pajsi #123', 'froilan@gmail.com', '466e95f1'),
+(55555, 'dadasd', 'sadsad', NULL, 'Av. America', 'sad@gmail.com', '8c5e4dc9'),
+(313131, 'Camilaxa', 'Perezxa', NULL, 'Av. Colombia', 'camixa2020@gmail.com', '216c6386'),
+(555666, 'Juanita', 'Ponce Armadil', '', '', 'juanita@gmail.com', '72546984'),
+(666999, 'Clarita', 'Suárez', NULL, 'Av. san pablo', 'clarita2010@gmail.com', '9dae3690'),
+(777888, 'Pedro', 'Canon Picaro', NULL, 'Av. Colombia', 'pedrito2000@gmailcom', '6d4e8def'),
+(9244226, 'Adriana', 'Quiroz', '', '', 'adriqy2005@gmail.com', '72062766'),
+(9258589, 'Martha', 'Yujra', NULL, 'Av. America', 'mys@gmail.com', 'cf0d1451'),
+(333555777, 'Rafael', 'De La Gueto', NULL, 'Calle wirma', 'rafita@gmail.com', 'aaf1d469'),
+(654654654, 'Fabricio', 'Cardenas', NULL, 'Calle Pajsi #123', 'fabrixxx@gmail.com', '0b4d0439'),
+(1020304050, 'Leo', 'Pacheco', NULL, 'Av. Colombia', 'leo@gmail.com', '8512f0f1'),
+(2147483647, 'Fabiola Carla', 'Yujra Quiroz', NULL, 'Av. Pabellon', 'fabyxs@gmail.com', '33e26a8f');
+
+-- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `productos`
@@ -194,7 +304,7 @@ CREATE TABLE `productos` (
 --
 
 INSERT INTO `productos` (`id_producto`, `nombre`, `descripcion`, `precio`, `stock`, `imagen`, `categoria`) VALUES
-(1, 'Pro Plan Adulto 3kg', 'Alimento balanceado premium para perros adultos', 185.00, 12, 'https://d34xtejqjqcp3x.cloudfront.net/store/d3ed875c04ea93a088da60d182535b9f.webp', 'Alimento'),
+(1, 'Pro Plan Adulto 3kg', 'Alimento balanceado premium para perros adultos', 185.00, 11, 'https://d34xtejqjqcp3x.cloudfront.net/store/d3ed875c04ea93a088da60d182535b9f.webp', 'Alimento'),
 (2, 'Royal Canin Gato 1.5kg', 'Nutrición específica para gatos domésticos', 135.00, 8, 'https://d34xtejqjqcp3x.cloudfront.net/store/f890525b942c7e3f9734f4126749fac6.webp', 'Alimento'),
 (3, 'Shampoo Neutro 500ml', 'Shampoo suave sin parabenos para todo tipo de pelo', 35.00, 20, 'https://d34xtejqjqcp3x.cloudfront.net/store/a963e1560fdc2390924170fd0c7d0bed.webp', 'Higiene'),
 (4, 'Cepillo Desmallador', 'Reduce enredos y caída de pelo eficientemente', 28.00, 15, 'https://m.media-amazon.com/images/S/aplus-media-library-service-media/4eb844b5-caae-46e4-8811-09d5e8da4419.__AC_SR166,182___.jpg', 'Higiene'),
@@ -267,7 +377,8 @@ INSERT INTO `veterinarios` (`carnetVet`, `especialidad`) VALUES
 ALTER TABLE `atenciones`
   ADD PRIMARY KEY (`id_atencion`),
   ADD KEY `carnetVet` (`carnetVet`),
-  ADD KEY `fk_atencion_cita` (`id_cita`);
+  ADD KEY `fk_atencion_cita` (`id_cita`),
+  ADD KEY `fk_atencion_mascota` (`id_mascota`);
 
 --
 -- Indices de la tabla `citas`
@@ -361,13 +472,13 @@ ALTER TABLE `atenciones`
 -- AUTO_INCREMENT de la tabla `citas`
 --
 ALTER TABLE `citas`
-  MODIFY `id_cita` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id_cita` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `clientes_mascotas`
 --
 ALTER TABLE `clientes_mascotas`
-  MODIFY `id_registroMasc` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id_registroMasc` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT de la tabla `colores`
@@ -379,7 +490,7 @@ ALTER TABLE `colores`
 -- AUTO_INCREMENT de la tabla `compras`
 --
 ALTER TABLE `compras`
-  MODIFY `id_compra` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_compra` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `especies`
@@ -391,7 +502,7 @@ ALTER TABLE `especies`
 -- AUTO_INCREMENT de la tabla `mascotas`
 --
 ALTER TABLE `mascotas`
-  MODIFY `id_mascota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id_mascota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
@@ -414,7 +525,8 @@ ALTER TABLE `razas`
 --
 ALTER TABLE `atenciones`
   ADD CONSTRAINT `atenciones_ibfk_2` FOREIGN KEY (`carnetVet`) REFERENCES `veterinarios` (`carnetVet`),
-  ADD CONSTRAINT `fk_atencion_cita` FOREIGN KEY (`id_cita`) REFERENCES `citas` (`id_cita`);
+  ADD CONSTRAINT `fk_atencion_cita` FOREIGN KEY (`id_cita`) REFERENCES `citas` (`id_cita`),
+  ADD CONSTRAINT `fk_atencion_mascota` FOREIGN KEY (`id_mascota`) REFERENCES `mascotas` (`id_mascota`);
 
 --
 -- Filtros para la tabla `citas`
